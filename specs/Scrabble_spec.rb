93c2aab0aac62a66a20c25c0aa88dddaa6b482e6
jm-rives <joanna.rives@gmail.com>
2016-08-30 11:29:39 -0700

    MERGE Files

--- specs/Scrabble_spec.rb
+++ specs/Scrabble_spec.rb
@@ -3,7 +3,8 @@ require_relative 'spec_helper.rb'
 require_relative '../Lib/Scrabble.rb'
 
 describe "Testing Scrabble" do |
-  it |
+  it "Must require Module Scrabble" do
+    expect()
   
 end
   
