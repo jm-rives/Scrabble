d0673f8d3638d2c796c091a8dc74f993b73549f5
jm-rives <joanna.rives@gmail.com>
2016-08-30 10:31:43 -0700

    ADD files from first pairing round

---
+++ lib/Scrabble.rb
@@ -0,0 +1,5 @@
+require_relative '../Scrabble.rb'
+
+class Scrabble::Scoring
+  
+end
---
+++ specs/Scrabble_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'spec_helper.rb'
+
+require_relative '../Lib/Scrabble.rb'
+
+describe "Testing Scrabble" do |
+  it |
+  
+end
+  
